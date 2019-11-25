be24668f930f097ef80f61741fc1b87df811a2a2
Will Szumski <will@stackhpc.com>
2019-11-25 16:34:58 +0000

    Add rally-verify-wrapper.sh

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,15 @@ FROM ubuntu:18.04
 
 RUN sed -i s/^deb-src.*// /etc/apt/sources.list
 
-RUN apt-get update && apt-get install --yes sudo python python-pip vim git-core && \
+RUN apt-get update && apt-get install --yes sudo python python-pip vim git-core crudini && \
     pip install --upgrade pip && \
     useradd -u 65500 -m rally && \
     usermod -aG sudo rally && \
     echo "rally ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/00-rally-user
 
-COPY . /home/rally/source
 COPY etc/motd /etc/motd
-WORKDIR /home/rally/source
+WORKDIR /opt/rundeck
+COPY bindep.txt /opt/rundeck/
 
 # ensure that we have all system packages installed
 RUN pip install bindep &&  apt-get install --yes $(bindep -b | tr '\n' ' ')
@@ -25,9 +25,17 @@ RUN echo '[ ! -z "$TERM" -a -r /etc/motd ] && cat /etc/motd' >> /etc/bash.bashrc
 # Cleanup pip
 RUN rm -rf /root/.cache/
 
+# Pre-download tempest to speed up runs
+RUN git clone --bare https://opendev.org/openstack/tempest /opt/tempest && \
+    pip install -r https://raw.githubusercontent.com/openstack/tempest/master/requirements.txt \
+    -c https://raw.githubusercontent.com/openstack/requirements/master/upper-constraints.txt
+
 USER rally
 ENV HOME /home/rally
-RUN mkdir -p /home/rally/data && rally db recreate
+RUN mkdir -p /home/rally/data && mkdir ~/.rally && cp /etc/rally/rally.conf ~/.rally/ && rally db recreate
+RUN rally verify create-verifier --name tempest --type tempest --system-wide --source /opt/tempest
+
+COPY bin/rally-verify-wrapper.sh /usr/bin/rally-verify-wrapper.sh
 
 # Docker volumes have specific behavior that allows this construction to work.
 # Data generated during the image creation is copied to volume only when it's
---
+++ bin/rally-verify-wrapper.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# This script assumes the following bind mounts
+# - An openrc file bind mounted to /home/rally/openrc
+# - A directory mount at /home/rally/artefacts
+# - (optional) a skip list mounted at /home/rally/skip-list
+# - (optional) a load list mounted at /home/rally/load-list 
+
+set -eux
+
+artefacts_dir=/home/rally/artefacts
+
+tempest_source=""
+if [ ! -z ${TEMPEST_SOURCE+x} ]; then
+    tempest_source="--source $TEMPEST_SOURCE"
+else
+    tempest_source="--source /opt/tempest"
+fi
+
+tempest_version=""
+if [ ! -z ${TEMPEST_VERSION+x} ]; then
+    tempest_version="--version $TEMPEST_VERSION"
+fi
+
+load_list=""
+# You can't have a load list and a pattern, pattern takes priority
+if [ -f ~/tempest-load-list ] && [ -z ${TEMPEST_PATTERN+x} ]; then
+    load_list="--load-list ~/tempest-load-list"
+fi
+
+skip_list=""
+if [ -f ~/tempest-skip-list ]; then
+    skip_list="--skip-list ~/tempest-skip-list"
+fi
+
+pattern=""
+if [ ! -z ${TEMPEST_PATTERN+x} ]; then
+   pattern="--pattern $TEMPEST_PATTERN"
+fi
+
+if [ ! -d $artefacts_dir ]; then
+    >&2 echo "You must mount a directory at $artefacts_dir"
+    exit -1
+fi
+
+# Don't print secrets
+set +x
+. ~/openrc
+set -x
+
+unset OS_CACERT
+
+mkdir ~/.rally/
+cp /etc/rally/rally.conf ~/.rally/
+
+crudini --set ~/.rally/rally.conf DEFAULT openstack_client_http_timeout 300
+
+rally deployment create --fromenv --name openstack
+
+rally verify create-verifier --name tempest --type tempest $tempest_source $tempest_version
+
+if [ -f ~/tempest-overrides.conf ]; then
+    rally verify configure-verifier --reconfigure --extend ~/tempest-overrides.conf
+fi
+
+rally verify start $skip_list $load_list $pattern \
+      > >(tee -a $artefacts_dir/stdout.log) 2> >(tee -a $artefacts_dir/stderr.log >&2)
+
+rally verify report --type html --to $artefacts_dir/rally-verify-report.html
