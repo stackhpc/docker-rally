736313dc37598f3282408380144aacef48e27309
Rafal Lewandowski <rafal@stackhpc.com>
2024-03-25 14:27:54 +0000

    fixtures fix

--- Dockerfile
+++ Dockerfile
@@ -5,21 +5,6 @@ RUN sed -i s/^deb-src.*// /etc/apt/sources.list
 ARG TEMPEST_SOURCE=https://github.com/openstack/tempest
 ARG TEMPEST_VERSION=master
 
-ARG TEMPEST_PLUGIN_OCTAVIA_SOURCE=https://github.com/stackhpc/octavia-tempest-plugin
-ARG TEMPEST_PLUGIN_OCTAVIA_VERSION=feature/non-admin
-
-ARG TEMPEST_PLUGIN_DESIGNATE_SOURCE=https://github.com/openstack/designate-tempest-plugin
-ARG TEMPEST_PLUGIN_DESIGNATE_VERSION=master
-
-ARG TEMPEST_PLUGIN_IRONIC_SOURCE=https://github.com/openstack/ironic-tempest-plugin
-ARG TEMPEST_PLUGIN_IRONIC_VERSION=master
-
-ARG TEMPEST_PLUGIN_MANILA_SOURCE=https://github.com/openstack/manila-tempest-plugin
-ARG TEMPEST_PLUGIN_MANILA_VERSION=master
-
-ARG TEMPEST_PLUGIN_MAGNUM_SOURCE=https://github.com/openstack/magnum-tempest-plugin
-ARG TEMPEST_PLUGIN_MAGNUM_VERSION=master
-
 RUN apt-get update && apt-get install --yes sudo python3-dev python3-pip vim git-core crudini jq iputils-ping && \
     apt clean && \
     pip3 --no-cache-dir install --upgrade pip setuptools && \
@@ -28,8 +13,9 @@ RUN apt-get update && apt-get install --yes sudo python3-dev python3-pip vim git
     echo "rally ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/00-rally-user && \
     mkdir /rally && chown -R rally:rally /rally
 
-RUN pip install git+https://github.com/stackhpc/rally-openstack.git@feature/non-admin-creds --no-cache-dir && \
-    pip3 install pymysql psycopg2-binary fixtures --no-cache-dir
+RUN pip install git+https://github.com/openstack/rally-openstack.git  --constraint https://raw.githubusercontent.com/openstack/rally-openstack/master/upper-constraints.txt --no-cache-dir && \
+    pip3 install pymysql psycopg2-binary --no-cache-dir
+    pip3 install fixtures
 
 COPY ./etc/motd_for_docker /etc/motd
 RUN echo '[ ! -z "$TERM" -a -r /etc/motd ] && cat /etc/motd' >> /etc/bash.bashrc
@@ -45,17 +31,8 @@ RUN rally db recreate
 
 RUN rally verify create-verifier --name default --type tempest --source $TEMPEST_SOURCE --version $TEMPEST_VERSION
 
-# For simplicitiy, always install common extensions
-RUN rally verify add-verifier-ext --source $TEMPEST_PLUGIN_OCTAVIA_SOURCE --version $TEMPEST_PLUGIN_OCTAVIA_VERSION
-RUN rally verify add-verifier-ext --source $TEMPEST_PLUGIN_DESIGNATE_SOURCE --version $TEMPEST_PLUGIN_DESIGNATE_VERSION
-RUN rally verify add-verifier-ext --source $TEMPEST_PLUGIN_IRONIC_SOURCE --version $TEMPEST_PLUGIN_IRONIC_VERSION
-RUN rally verify add-verifier-ext --source $TEMPEST_PLUGIN_MANILA_SOURCE --version $TEMPEST_PLUGIN_MANILA_VERSION
-RUN rally verify add-verifier-ext --source $TEMPEST_PLUGIN_MAGNUM_SOURCE --version $TEMPEST_PLUGIN_MAGNUM_VERSION
-
 COPY bin/rally-verify-wrapper.sh /usr/bin/rally-verify-wrapper.sh
 COPY bin/rally-extract-tests.sh /usr/bin/rally-extract-tests.sh
-COPY bin/rally-normalize.py /usr/bin/rally-normalize.py
-COPY bin/test_server.bin /opt/octavia-tempest-plugin/test_server.bin
 
 # Data generated during the image creation is copied to volume only when it's
 # attached for the first time (volume initialization)
--- bin/rally-verify-wrapper.sh
+++ bin/rally-verify-wrapper.sh
@@ -11,39 +11,10 @@ set -eux
 
 artifacts_dir=/home/rally/artifacts
 
-if [ ! -z ${TEMPEST_NO_PROXY_EXTRA:+x} ]; then
-    if [ -z ${NO_PROXY:+x} ]; then
-        NO_PROXY="$TEMPEST_NO_PROXY_EXTRA"
-    else
-        NO_PROXY="$NO_PROXY,$TEMPEST_NO_PROXY_EXTRA"
-    fi
-    if [ -z ${no_proxy:+x} ]; then
-        no_proxy="$TEMPEST_NO_PROXY_EXTRA"
-    else
-        no_proxy="$no_proxy,$TEMPEST_NO_PROXY_EXTRA"
-    fi
-fi
-
-if [ ! -z ${TEMPEST_LOAD_LIST:+x} ]; then
-    echo "$TEMPEST_LOAD_LIST" > ~/tempest-load-list
-fi
-
-if [ ! -z ${TEMPEST_SKIP_LIST:+x} ]; then
-    echo "$TEMPEST_SKIP_LIST" > ~/tempest-skip-list
-fi
-
-if [ ! -z ${TEMPEST_CONF_OVERRIDES:+x} ]; then
-    echo "$TEMPEST_CONF_OVERRIDES" > ~/tempest-overrides.conf
-fi
-
 load_list=""
 # You can't have a load list and a pattern, pattern takes priority
 if [ -f ~/tempest-load-list ] && [ -z ${TEMPEST_PATTERN:+x} ]; then
     load_list="--load-list /home/rally/tempest-load-list"
-    if [ $(wc -l /home/rally/tempest-load-list | cut -d ' ' -f 1) -lt 1]; then
-        echo >&2 "The load list appears to be empty, exiting..."
-        exit -1
-    fi
 fi
 
 skip_list=""
@@ -85,14 +56,7 @@ crudini --set ~/.rally/rally.conf openstack flavor_ref_ram 128
 crudini --set ~/.rally/rally.conf openstack flavor_ref_alt_ram 256
 crudini --set ~/.rally/rally.conf openstack flavor_ref_disk 1
 crudini --set ~/.rally/rally.conf openstack flavor_ref_alt_disk 1
-# Use a StackHPC-built image without a default password.
-crudini --set ~/.rally/rally.conf openstack img_url https://github.com/stackhpc/cirros/releases/download/20231206/cirros-d231206-x86_64-disk.img
-
-# WARNING: This option relies on a custom fork for rally-openstack and should
-# be considered unstable
-if [ ! -z ${RALLY_CONF_ENABLE_CREATE_TEMPEST_RESOURCES:+x} ]; then
-    crudini --set ~/.rally/rally.conf openstack enable_create_resources "${RALLY_CONF_ENABLE_CREATE_TEMPEST_RESOURCES}"
-fi
+crudini --set ~/.rally/rally.conf openstack img_url http://download.cirros-cloud.net/0.5.1/cirros-0.5.1-x86_64-disk.img
 
 rally deployment create --fromenv --name openstack
 
@@ -100,17 +64,7 @@ if [ -f ~/tempest-overrides.conf ]; then
     rally verify configure-verifier --reconfigure --extend ~/tempest-overrides.conf
 fi
 
-if [ -f ~/tempest-load-list ] && [ -z ${TEMPEST_PATTERN:+x} ]; then
-    if [ ${TEMPEST_NORMALIZE_LOAD_LIST:-1} -eq 1 ]; then
-        echo normalizing load-list
-        rally-normalize.py /home/rally/tempest-load-list
-    fi
-    if [ $(wc -l /home/rally/tempest-load-list | cut -d ' ' -f 1) -lt 1 ]; then
-        echo >&2 "The load list appears to be empty, exiting..."
-        exit -1
-    fi
-fi
-
+failed=0
 rally verify start $skip_list $load_list $pattern $concurrency > >(tee -a $artifacts_dir/stdout.log) 2> >(tee -a $artifacts_dir/stderr.log >&2) || export failed=1
 
 rally verify report --type html --to $artifacts_dir/rally-verify-report.html
@@ -122,3 +76,7 @@ rally-extract-tests.sh --status fail >$artifacts_dir/failed-tests
 # NOTE: this assumes only one of these files exists which should ordinarily
 # be the case when the container is discarded after one run.
 find ~/.rally -name "tempest.log" -print -exec cp {} $artifacts_dir/ \;
+
+if [ $failed == 1 ]; then
+   exit -1
+fi
